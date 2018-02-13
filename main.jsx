e32450b4ae7780fddca03f465f604b00e251683c
Matt <mmszek3000@gmail.com>
2018-02-13 17:01:03 -0500

    Add a lame NavBar

--- main.jsx
+++ main.jsx
@@ -1,3 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Sizzle from 'sizzle';
+import './main.css';
+
+class NavBar extends React.Component {
+    render() {
+        return (
+            <div id="nav">
+                <a href="/" class="nav-item" id="home">Home</a>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <NavBar />,
+    Sizzle('body')
+);
