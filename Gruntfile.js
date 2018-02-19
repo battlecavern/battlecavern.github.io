9337ce039660d33509fc0a854127efd80de4d726
Matt <mmszek3000@gmail.com>
2018-02-19 09:28:25 -0500

    Add minification and re-configure Grunt and Travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,29 @@
-const webpackConfig = require('./webpack.config')
-
 module.exports = function (grunt) {
   grunt.initConfig({
     eslint: {
       target: ['*.js', '*.jsx']
     },
-    watch: {
-      files: ['*.js', '*.jsx', '*.css'],
-      tasks: ['eslint', 'webpack']
-    },
     webpack: {
-      server: Object.assign({ watch: true }, webpackConfig)
+      compile: require('./webpack.config')
+    },
+    uglify: {
+      options: {
+        banner: `/**
+ * BattleCavern Source Code
+ * Copyright (c) 2018 BattleCavern
+ * @license GPL-3.0
+ */`
+      },
+      build: {
+        src: 'bundle.js',
+        dest: 'bundle.js'
+      }
     }
   })
 
   grunt.loadNpmTasks('grunt-eslint')
   grunt.loadNpmTasks('grunt-webpack')
+  grunt.loadNpmTasks('grunt-contrib-uglify-es')
 
-  grunt.registerTask('default', ['eslint', 'webpack'])
+  grunt.registerTask('default', ['eslint', 'webpack', 'uglify'])
 }
